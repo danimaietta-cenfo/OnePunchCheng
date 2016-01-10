5194c7949419d1d6a440314071a254ec2314b406
danimaietta <dmaiettae@ucenfotec.ac.cr>
2016-01-09 18:38:52 -0600

    Index fianlizado

--- logic/controllers/mainController.js
+++ logic/controllers/mainController.js
@@ -2,26 +2,30 @@ app.controller('mainController',['$scope', function($scope){
   $scope.framework = ' Im angular js';
   $scope.intro = 'This part of angular we create the necessary variables and functions to the page';
   $scope.total = 0;
+  $scope.angularImage = 'img/angularjs-logo.png';
   $scope.list = [
     {
   	  name: 'AngularJs',
   	  whatIs: 'Im a javascript framework',
+      syntax: 'Directives and Expressiones',
   	  number: 9
     },
     {
       name: 'Directives',
    	  whatIs: 'AngularJs extends (hereda) HTML attributes with Directives',
-   	  number: 11
+   	  syntax: 'ng-something="" ',
+      number: 11
     },
     {
       name: 'Expressions',
    	  whatIs: 'Binds data to HTML with Expressions',
+      syntax: '{{}}',
    	  number: 7
     }
   ];
 
-  $scope.plus = function(index){
-    $scope.total += $scope.list[index].number;
+  $scope.plus = function(){
+    $scope.total += $scope.list[1].number;
   };  
  
 }]);
